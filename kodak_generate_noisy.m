a40731beab667cd83057898cf2fca4d978825a8d
Yuqi Li <liyuqi14ab14@gmail.com>
2018-04-17 21:20:06 -0500

    ..

--- kodak_generate_noisy.m
+++ kodak_generate_noisy.m
@@ -12,25 +12,29 @@ for idxDS = 1:length(DatasetList)
 	numImage = numel(imList);
 	for idxImage = 1:numImage
 		curData     =   imList(idxImage).name;
-                curName     =   curData(1:end - length('.mat'))
-                load(fullfile(imageDir,curData),'X');
-                noisyDir = fullfile(imageDir,curName);
-                mkdir(noisyDir)
-                for i = 1:length(NoiseLevelList)
-                	sigma = NoiseLevelList(i);
-                	% generate .mat
-                	noisy = X + sigma*randn(size(X));
-                	psnr_noisy = PSNR(noisy-X);
-                	outfile = sprintf('%s/sigma%d.mat', char(noisyDir),sigma);
-                	save(outfile,'noisy','psnr_noisy');
+                curName     =   curData(1:end - length('.mat')) 
+                load(fullfile(imageDir,curData),'I7');
+                X = I7;
+                save(char(fullfile(imageDir,curData)),'X');
 
-                	% generate .png
-                	noisyPngDir = fullfile(imageDir,sprintf('noisy_sigma%d',sigma));
-                	if ~exist(noisyPngDir,'dir')
-                		mkdir(noisyPngDir);
-                	end
-                	outfile = sprintf('%s/%s_sigma%d.png',noisyPngDir,curName,sigma);
-                	imwrite(uint8(noisy), outfile);
-                end
+                % load(fullfile(imageDir,curData),'X');
+                % noisyDir = fullfile(imageDir,curName);
+                % mkdir(noisyDir)
+                % for i = 1:length(NoiseLevelList)
+                % 	sigma = NoiseLevelList(i);
+                % 	% generate .mat
+                % 	noisy = X + sigma*randn(size(X));
+                % 	psnr_noisy = PSNR(noisy-X);
+                % 	outfile = sprintf('%s/sigma%d.mat', char(noisyDir),sigma);
+                % 	save(outfile,'noisy','psnr_noisy');
+
+                % 	% generate .png
+                % 	noisyPngDir = fullfile(imageDir,sprintf('noisy_sigma%d',sigma));
+                % 	if ~exist(noisyPngDir,'dir')
+                % 		mkdir(noisyPngDir);
+                % 	end
+                % 	outfile = sprintf('%s/%s_sigma%d.png',noisyPngDir,curName,sigma);
+                % 	imwrite(uint8(noisy), outfile);
+                % end
 	end
 end
