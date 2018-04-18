273b46395b0f348720ce0451ad4b4e41b500e1d5
Yuqi Li <liyuqi14ab14@gmail.com>
2018-04-17 20:25:01 -0500

    ..

--- denoising_ksvd.m
+++ denoising_ksvd.m
@@ -6,7 +6,7 @@ NoiseLevelList 	=	[5, 10, 15, 25, 50];
 
 addpath('KSVD_Matlab_ToolBox');
 bb = 8; % block size
-RRList = [1, 2, 8]; % redundancy factor
+RRList = [2, 8]; % redundancy factor
 % RRList = [4];
 
 method = 'ksvd';
